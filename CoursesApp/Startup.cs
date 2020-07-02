6fe9c277c56b3173b3cde048dabb6ad3a11be7b6
Andreea <coroian.andreea@gmail.com>
2020-07-02 21:55:48 +0300

    Added swagger

--- CoursesApp/Startup.cs
+++ CoursesApp/Startup.cs
@@ -16,6 +16,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System.IO;
 
 namespace CoursesApp
 {
@@ -41,11 +43,45 @@ namespace CoursesApp
                 .AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
             services.AddDbContext<CoursesDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("CoursesDbConnectionString")));
 
+            //Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "My Courses API",
+                    Description = "A simple example ASP.NET Core Web Courses API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Andreea Coroian",
+                        Email = "coroian.andreea@gmail.com",
+                        Url = new Uri("https://www.facebook.com/andreea.coroian.3010"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+           {
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Courses API V1");
+           });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
