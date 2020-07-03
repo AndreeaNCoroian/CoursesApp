58e1cec1ccd56e2d088405d1b089732951c7260a
Andreea <coroian.andreea@gmail.com>
2020-07-03 13:04:55 +0300

    changes done on review entity

--- CoursesApp/Models/Review.cs
+++ CoursesApp/Models/Review.cs
@@ -8,7 +8,6 @@ namespace CoursesApp.Models
     public class Review
     {
         public long id { get; set; }
-        public object Id { get; internal set; }
         public string Content { get; set; } 
         public long CourseId { get; set; }
         public Course Course { get; set; }
